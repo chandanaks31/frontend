87045fb75b4fb77c4df45fc4ba408126c022f4bc
Chandana K S <chandanakschandana31@gmail.com>
2025-01-29 11:49:58 +0530

    sum of natural number

---
+++ program.java
@@ -0,0 +1,14 @@
+
+class program {
+
+    public static void main(String[] args) {
+        int i = 1;
+        int n = 5;
+        int sum = 0;
+        while (i <= n) {
+            sum = sum + i;
+            i++;
+        }
+        System.out.println("sum");
+    }
+}
