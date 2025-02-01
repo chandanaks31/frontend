ecdec51aed53649dbbd8c552ae3164cdd3b20b7f
Chandana K S <chandanakschandana31@gmail.com>
2025-02-01 12:11:27 +0530

    positive num

---
+++ Statment.java
@@ -0,0 +1,12 @@
+public class Statment {
+    public static void main(String[] args) {
+            int num=-5;
+            if(num==0)
+            System.out.println("it is zero");
+            else if(num>0)
+            System.out.println(num+"is positive");
+            else 
+            System.out.println(num+"is negative");
+    
+    }
+}
